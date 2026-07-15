362eff1cf9b3d1bc583ed8278eb469e3b8d7ff34
hello0zai <hello0zaizai@gmail.com>
2026-07-15 16:44:57 +0700

    build: compile Python modules to .pyd with Cython for code obfuscation

---
+++ sd_watcher_window/setup.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import os
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+BASE = Path(__file__).resolve().parent
+os.chdir(BASE)
+
+extensions = [
+    Extension("config", ["config.py"]),
+    Extension("lib", ["lib.py"]),
+    Extension("main", ["main.py"]),
+    Extension("main", ["main.py"]),    
+]
+
+
+setup(
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            "language_level": "3",           
+        },
+        
+    )
+)
